dcc85af1943a3f062b2ebe24d4365165d17c9f0b
Problemon <danilets.pasha@mail.ru>
2021-06-01 11:00:19 +1000

    big update

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let buttonOpen = document.querySelector('.info__btn'); // Кнопка "обратная связь" которая открывает иодальное окно.
+let modal = document.querySelector('.modal-fade'); // Оверлэй модального окна, вместе с модальным окном.
+let buttonClose = document.querySelector('.modal__btn-close'); // Кнопка закрытия модального окна.
+
+buttonOpen.onclick = function () {
+    modal.style.display = "block";
+};
+
+buttonClose.onclick = function () {
+    modal.style.display = "none";
+};
